68288501b607fb312e1b2a97e37def0dcb0772b6
Mohmadkahal22 <mohmadalkahal@gmail.com>
2025-08-20 08:04:14 -0500

    .
    .

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,8 @@
 // config.js
 window.API_CONFIG = {
-    BASE_URL: 'http://localhost:8000', 
+    BASE_URL_Renter: 'http://localhost:8000', 
+    BASE_URL_Uber: 'http://doosdoos.com/api/v1', 
+
     ENDPOINTS: {
         LOGIN: '/api/login', 
     },
