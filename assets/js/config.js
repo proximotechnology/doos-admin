dfad282b0ad35f4a8d470f0017c6691bb3799f5b
Mohmadkahal22 <mohmadalkahal@gmail.com>
2025-09-09 12:29:16 +0300

    .
    .

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,8 +1,8 @@
 // config.js
 window.API_CONFIG = {
-    BASE_URL_Renter: 'http://localhost:8000', 
+    // BASE_URL_Renter: 'http://localhost:8000', 
 
-    // BASE_URL_Renter: 'https://api.doosdoostest.com', 
+    BASE_URL_Renter: 'https://api.doosdoostest.com', 
     BASE_URL_Uber: 'https://api.doosdoos.com/api/v1', 
 
     ENDPOINTS: {
